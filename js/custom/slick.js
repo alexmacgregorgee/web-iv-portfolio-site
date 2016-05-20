effe4716316bc99fba0ddd686bec786565948351
Alex Gee <alexmacgregorgee@gmail.com>
2016-05-20 13:24:11 -0500

    Fixing buttons. Sort of.

--- js/custom/slick.js
+++ js/custom/slick.js
@@ -1,10 +1,8 @@
     $('.one-time').slick({
-  arrows: true,
   infinite: true,
-  dots: true,
   speed: 300,
   slidesToShow: 1,
   slidesToScroll: 1,
-  prevArrow: $ ('.prev'),
-  nextArrow: $ ('.next')
+  arrows: true,
+  dots: false, 
 });
